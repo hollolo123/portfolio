f7e07364afba6da74c98dc594f01bc26be3847f9
hollolo123 <hollolo123@gmail.com>
2025-10-17 12:14:07 +0900

    design :: project:

--- src/data/project/project.js
+++ src/data/project/project.js
@@ -9,7 +9,7 @@ const projectData = [
     name : "포트폴리오",
     type : "personal",
     keyword : ["Vue.js", "SCSS", "Vite", "Vercel"],
-    description : "포트폴리오",
+    description : "포트폴리오 사이트 입니다. Vue.js를 사용하여 만들었습니다.",
     thumbnail: proj_img01,
     url : "https://surimpark.vercel.app/",
     github : "https://github.com/hollolo123/portfolio"
@@ -18,9 +18,9 @@ const projectData = [
     id : 2,
     name : "대전세잔직업전문학교 리뉴얼",
     type : "team",
-    keyword : ["HTML", "SCSS", "JavaScript", "JQuery", "Parcel", "Vercel"],
+    keyword : ["HTML", "SCSS", "JavaScript", "JQuery", "Vercel"],
     thumbnail: proj_img02,
-    description : "팀 프로젝트",
+    description : "팀 프로젝트로 진행한 리뉴얼 사이트입니다.",
     url: "https://renewal-cezanne.vercel.app/",
     github : "https://github.com/JeongJae1203/renewal-cezanne"
   },
@@ -30,7 +30,7 @@ const projectData = [
     type : "personal",
     keyword : ["Vue.js", "SCSS", "Vite", "Vercel"],
     thumbnail : proj_img03,
-    description : "vue.js compile",
+    description : "리뉴얼 사이트를 개인프로젝트로 Vue.js를 사용하여 만들었습니다.",
     url : "https://cezanne-vue.vercel.app/",
     github : "https://github.com/hollolo123/cezanne_vue"
   }
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -4,22 +4,34 @@
       <div class="inner">
         <Title title="Project" />
         <div class="proj__wrapper">
-          <div 
-            v-for="project in projectData" 
-            :key="project.id" 
-            class="proj__box">
-            <div class="item">
-              <a :href="project.url" target="_blank">
-                <div class="proj__thumbnail">
-                  <img :src="project.thumbnail" :alt="project.name">
-                </div>
-                <div class="proj__title">
-                  <p class="tit">{{ project.name }}</p>
-                  <p class="skill">
-                    {{ project.keyword.join(', ') }}
-                  </p>
-                </div>
-              </a>
+          <div class="proj__tab">
+            <ul>
+              <li class="active">All</li>
+              <li>Team</li>
+              <li>Personal</li>
+            </ul>
+          </div>
+          <div class="proj__box__wrapper">
+            <div 
+              v-for="project in projectData" 
+              :key="project.id" 
+              class="proj__box">
+              <div class="item">
+                <a :href="project.url" target="_blank">
+                  <div class="proj__thumbnail">
+                    <img :src="project.thumbnail" :alt="project.name">
+                  </div>
+                  <div class="proj__title">
+                    <p class="tit">{{ project.name }}</p>
+                    <p class="desc">{{ project.description }}</p>
+                    <div class="skill">
+                      <span v-for="keyword in project.keyword" 
+                      :key="keyword">{{ keyword }}</span>
+                    </div>
+                    <button class="btn_site">사이트 바로가기</button>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -40,7 +52,8 @@ export default {
     return {
       projectData
     }
-  }
+  },
+  
 }
 </script>
 
