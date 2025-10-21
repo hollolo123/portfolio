e202418c6e08e16c9b16c7df679ad5852a9baa3a
hollolo123 <hollolo123@gmail.com>
2025-10-21 10:56:02 +0900

    작업중인 변경사항 저장

--- src/views/Comment.vue
+++ src/views/Comment.vue
@@ -2,7 +2,7 @@
   <section class="comment">
     <div class="contents">
       <div class="inner">
-        <p>Love is beautiful, but it hurts.</p>
+        <h2>Love is beautiful, but it hurts.</h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.<br/>
           Similique debitis libero animi? Aspernatur, in et placeat enim autem quasi unde cumque,<br/>
           quis aliquid numquam asperiores! Magnam vitae unde temporibus soluta?</p>
